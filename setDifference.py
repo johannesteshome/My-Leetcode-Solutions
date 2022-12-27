688687df604e94788a4b72f9fc41685837049ac7
Yohannes Teshome <83913756+John-droid-dotcom@users.noreply.github.com>
2022-12-27 11:05:38 +0300

    Create setDifference.py

---
+++ setDifference.py
@@ -0,0 +1,9 @@
+
+englishStudents = int(input())
+englishStudentsSet = set(map(int, input().split()))
+frenchStudents = int(input())
+frenchStudentsSet = set(map(int, input().split()))
+
+onlyEnglish = englishStudentsSet.difference(frenchStudentsSet)
+
+print(len(onlyEnglish))
