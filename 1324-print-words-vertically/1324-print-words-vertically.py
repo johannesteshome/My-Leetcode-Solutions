608d68faa273defffb8c0e826f8d40af1a14ad3e
Yohannes Teshome <83913756+John-droid-dotcom@users.noreply.github.com>
2023-01-04 19:48:39 +0300

    Time: 56 ms (46.03%), Space: 13.8 MB (98.15%) - LeetHub

--- 1324-print-words-vertically/1324-print-words-vertically.py
+++ 1324-print-words-vertically/1324-print-words-vertically.py
@@ -2,10 +2,9 @@ class Solution:
     def printVertically(self, s: str) -> List[str]:
         arrStr = s.split()
         
-        print(arrStr)
         crossStr = list(itertools.zip_longest(*arrStr, fillvalue =' ' ))
-        print(crossStr)
+        
         for i in range(len(crossStr)):
             crossStr[i] = "".join(crossStr[i]).rstrip()
-        print(crossStr)
+        
         return crossStr
