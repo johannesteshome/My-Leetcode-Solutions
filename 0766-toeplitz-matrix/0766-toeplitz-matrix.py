e0516f85aa6758dcf069b101edbcd32a1c58f71c
Yohannes Teshome <83913756+John-droid-dotcom@users.noreply.github.com>
2023-01-13 18:36:34 +0300

    Time: 102 ms (52.08%), Space: 13.9 MB (34.95%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -1,10 +1,13 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        #creating the dictionary
         diagonals = defaultdict(list)
+        #populating the dictionary with the values of the subtracttion of their index
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 diagonals[i-j].append(matrix[i][j])
                 
+        #traversing the dictionary to check if they fulfill the condition
         for value in diagonals.values():
             res = value.count(value[0]) == len(value)
             if(not res):
