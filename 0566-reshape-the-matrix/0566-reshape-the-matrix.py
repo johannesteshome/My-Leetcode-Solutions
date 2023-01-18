d1f4864bf8af5fc6cdbd25ad76579ae0e2dcb12b
Yohannes Teshome <83913756+John-droid-dotcom@users.noreply.github.com>
2023-01-18 12:54:38 +0300

    Time: 84 ms (96.12%), Space: 14.8 MB (32.18%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.py
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.py
@@ -1,38 +1,26 @@
 class Solution:
     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
-        # rows = r
-        # cols = c
-        
-        # if(rows < cols):
-        #     answer = [[0*cols] for _ in range(rows) ]
-        # else:
-        #     answer = [[0]*cols for _ in range(rows)]
-        # print(answer)
-        
+      
         answers = []
 
         if(r*c != len(mat)*len(mat[0])):
             return mat
-        else:
-            R = 0
-            C = 0
-            
-            answer = []
-            for i in range(len(mat)):
-                for j in range(len(mat[i])):
-                    if(C < c):
-                        print(R,C, "first if")
-                        answer.append(mat[i][j])
-                        C = C + 1
-                    elif(C == c):
-                        answers.append(answer)
-                        C = 0
-                        R = R + 1
-                        answer = []
-                        answer.append(mat[i][j])
-                        C = C +1
-                        print(R, C, "elif")
-            answers.append(answer)
-            print(answers)
-            return answers
-                        
+
+        R = 0
+        C = 0
+
+        answer = []
+        for i in range(len(mat)):
+            for j in range(len(mat[i])):
+                if(C < c):
+                    answer.append(mat[i][j])
+                    C = C + 1
+                elif(C == c):
+                    answers.append(answer)
+                    C = 0
+                    R = R + 1
+                    answer = []
+                    answer.append(mat[i][j])
+                    C = C +1
+        answers.append(answer)
+        return answers
