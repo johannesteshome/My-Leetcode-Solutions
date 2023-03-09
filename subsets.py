5c34d25275f455a81a91f1f24237fd15c7147dc3
Yohannes Teshome <83913756+John-droid-dotcom@users.noreply.github.com>
2023-03-09 16:56:54 +0300

    added subsets.python

---
+++ subsets.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+
+        def backtrack(idx, arr):
+            ans.append(arr)
+            # if len(ans) == 2**len(nums):
+            #     return
+
+            for i in range(idx, len(nums)):
+                backtrack(i+1, arr+[nums[i]])
+        
+        backtrack(0, [])
+        return ans
