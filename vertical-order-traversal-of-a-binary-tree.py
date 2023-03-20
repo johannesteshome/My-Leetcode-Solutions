31a0f6da1b64abe4b95293566aed2a09a86a44a2
Yohannes Teshome <83913756+John-droid-dotcom@users.noreply.github.com>
2023-03-20 22:57:24 +0300

    added vertical-order-traversal-of-a-binary-tree.python

--- vertical-order-traversal-of-a-binary-tree.py
+++ vertical-order-traversal-of-a-binary-tree.py
@@ -12,44 +12,22 @@ class Solution:
         def vert(root, pos):
             if not root:
                 return 
-            # print(Map, newMap)
-            # if Map[(pos[0], pos[1])] == []:
-            #     Map[(pos[0], pos[1])].append(root.val)
-            #     newMap[pos[1]].append(root.val)
-            # else:
-            #     newMap[pos[1]].remove(Map[(pos[0], pos[1])][0])
-            #     Map[(pos[0], pos[1])].append(root.val)
-            #     Map[(pos[0], pos[1])].sort()
-            #     newMap[pos[1]].extend(Map[(pos[0], pos[1])])
+           
+           # store the values on a dictionary where the key is (col, row)
             Map[(pos[1], pos[0])].append(root.val)
             if len(Map[(pos[1], pos[0])]) > 1:
                 Map[(pos[1], pos[0])].sort()
-
-                
 
             vert(root.left, [pos[0]+1, pos[1]-1])
             vert(root.right, [pos[0]+1, pos[1]+1])
         
         vert(root, [0,0])
-        # print(Map)
-        # for m in Map:
-        #     # print(m[1])
-        #     if len(Map[m]) == 1:
-        #         newMap[int(m[1])].extend(Map[m])
-        #         newMap
-        #     else:
-        #         Map[m].sort()
-        #         newMap[int(m[1])].extend(Map[m])
+        
+        # sort the dictionary based on the column
         nMap = OrderedDict(sorted(Map.items()))
-        print(nMap)
 
+        # store the lists with the same column in another dictionary
         for n in nMap:
             newMap[int(n[0])].extend(nMap[n])
 
-        # return newMap.values()
-        
-        
-        # for item in Map.values():
-        #     item.sort()
-        
         return list(newMap.values())
