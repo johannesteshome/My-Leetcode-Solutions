05445aca5dab41ed55c59c50ef25d5fca7c5f741
Yohannes Teshome <83913756+John-droid-dotcom@users.noreply.github.com>
2023-03-22 13:42:10 +0300

    added arrayManipulation.py

---
+++ arrayManipulation.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'arrayManipulation' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def arrayManipulation(n, queries):
+    # Write your code here
+    n += 2
+    arr = [0] * n
+    Max = 0
+    for interval in queries:
+        arr[interval[0]] += interval[2]
+        arr[interval[1]+1] -= interval[2]
+    temp = 0
+    for index in range(len(arr)):
+        temp += arr[index]
+        Max = max(Max, temp)
+    return Max
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
