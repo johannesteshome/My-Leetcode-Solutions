046960c95bf26eed3a859e196c2bf52836b00e4f
Yohannes Teshome <83913756+John-droid-dotcom@users.noreply.github.com>
2023-03-24 18:38:27 +0300

    added find-all-numbers-disappeared-in-an-array.python

---
+++ find-all-numbers-disappeared-in-an-array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        i = 0
+        Map = defaultdict(list)
+        answer = []
+        while i < n:
+            correct_position = nums[i] -1
+            if correct_position != i:
+                if nums[correct_position] == nums[i]:
+                    Map[nums[i]] = i+1
+                    i+=1
+                else:
+                    nums[correct_position], nums[i] = nums[i], nums[correct_position]
+            else:
+                i += 1
+        print(nums)
+        return Map.values()
