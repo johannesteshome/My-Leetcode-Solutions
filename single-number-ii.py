2f4a4744ea41502f278d6c4a0bbd588c43e1b4d1
Yohannes Teshome <83913756+John-droid-dotcom@users.noreply.github.com>
2023-04-05 18:12:30 +0300

    added single-number-ii.python

--- single-number-ii.py
+++ single-number-ii.py
@@ -1,17 +1,21 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        found = False
-        for i in range(len(nums)-1):
-            if nums[i] ^ nums[i+1] == 0:
-                if found:
-                    found = False 
-                continue
-            else:
-                if i == 0:
-                    return nums[0]
-                if found:
-                    return nums[i]
-                found = True
-        
-        return nums[-1]
+        answer = 0
+        negativeCount = 0
+    
+        for num in nums:
+            if num < 0:
+                negativeCount += 1
+
+        for i in range(32):
+            count = 0
+            for num in nums:
+                if num < 0:
+                    num = abs(num)
+                if num & (1<<i) != 0:
+                    count += 1
+            print(count)
+            if count % 3 != 0:
+                answer |= (1<<i)
+            
+        return answer if negativeCount % 3 == 0 else -1 * answer
