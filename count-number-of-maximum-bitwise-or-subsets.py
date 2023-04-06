5f65a8ec54adae116108861d2a6382a10e24cfaf
Yohannes Teshome <83913756+John-droid-dotcom@users.noreply.github.com>
2023-04-06 18:38:14 +0300

    added count-number-of-maximum-bitwise-or-subsets.python

--- count-number-of-maximum-bitwise-or-subsets.py
+++ count-number-of-maximum-bitwise-or-subsets.py
@@ -1,30 +1,25 @@
 class Solution:
     def countMaxOrSubsets(self, nums: List[int]) -> int:
+        bit = 0
         Max = 0
         count = 0
+        for i in range(len(nums)):
+            bit ^= (1<<i)
+        # print(bit)
 
-        def backtrack(arr, i):
-            # print(arr)
-            nonlocal Max
-            nonlocal count
-            # print(count)
-
+        for i in range(bit+1):
             res = 0
-            for k in range(len(arr)):
-                res |= arr[k]
-            
+            for j in range(32):
+                if i & (1<<j) != 0:
+                    res |= nums[j]
+
             if res > Max:
                 Max = res
                 count = 1
             elif res == Max:
                 count += 1
-            
-            if len(arr) == len(nums):
-                return
-
-            for j in range(i+1, len(nums)):
-                backtrack(arr+[nums[j]], j)
         
-        backtrack([], -1)
-
         return count
+
+
+        # return bit
