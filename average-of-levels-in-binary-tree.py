09e996eea32850c2972ad1f109eecc5d0d2da335
Yohannes Teshome <83913756+John-droid-dotcom@users.noreply.github.com>
2023-04-28 17:13:36 +0300

    added average-of-levels-in-binary-tree.python

--- average-of-levels-in-binary-tree.py
+++ average-of-levels-in-binary-tree.py
@@ -12,26 +12,21 @@ class Solution:
 
         queue.append(root)
         # visited.add(root)
-        answer.append(root.val)
+        Sum = root.val
 
         while queue:
-            count = 0
+            answer.append(Sum/len(queue))
             Sum = 0
             level = len(queue)
-            
 
             for i in range(level):
                 node = queue.popleft()
                 if node.left:
                     queue.append(node.left)
                     Sum += node.left.val
-                    count += 1
                 if node.right:
                     queue.append(node.right)
                     Sum += node.right.val
-                    count += 1
 
-            if count > 0:
-                answer.append(Sum/count)
         
         return answer
