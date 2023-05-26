679fd934c93a4485e06d083d1dff723fb7a12aaf
Yohannes Teshome <83913756+johannesteshome@users.noreply.github.com>
2023-05-26 17:59:25 +0300

    added fibonacci-number.python

--- fibonacci-number.py
+++ fibonacci-number.py
@@ -1,8 +1,12 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
-        elif n==1:
-            return 1
-        else:
-            return self.fib(n-1) + self.fib(n-2)
+        mem = defaultdict(int)
+
+        def fibo(n):
+            if n == 0 or n==1:
+                return n
+            elif n not in mem:
+                mem[n] = fibo(n-1) + fibo(n-2)
+            return mem[n]
+        
+        return fibo(n)
