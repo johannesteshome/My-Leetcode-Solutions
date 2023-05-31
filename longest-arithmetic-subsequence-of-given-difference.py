c7574e446910334622cf3496ed4c75c82d80169a
Yohannes Teshome <83913756+johannesteshome@users.noreply.github.com>
2023-05-31 23:45:33 +0300

    added longest-arithmetic-subsequence-of-given-difference.python

--- longest-arithmetic-subsequence-of-given-difference.py
+++ longest-arithmetic-subsequence-of-given-difference.py
@@ -3,11 +3,13 @@ class Solution:
         memo = {}
 
         memo[arr[0]] = 1
+        Max = 1
 
         for i in range(1, len(arr)):
             if arr[i] - difference in memo:
                 memo[arr[i]] = 1 + memo[arr[i] - difference]
+                Max = max(memo[arr[i]], Max)
             else:
                 memo[arr[i]] = 1
 
-        return max(memo.values())
+        return Max
