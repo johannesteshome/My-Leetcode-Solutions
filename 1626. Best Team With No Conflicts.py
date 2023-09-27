4c5a599d4beeae6c27dd9e231fd1d018f46229ba
Yohannes Teshome <83913756+johannesteshome@users.noreply.github.com>
2023-09-27 16:25:58 +0300

    Create 1626. Best Team With No Conflicts.py

---
+++ 1626. Best Team With No Conflicts.py
@@ -0,0 +1,19 @@
+class Solution:
+    def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
+        combined = list(zip(ages, scores))
+        combined = sorted(combined)
+        print(combined)
+
+        dp = [combined[i][1] for i in range(len(scores))]
+        print(dp)
+
+        for i in range(len(scores)-1, -1, -1):
+            for j in range(i+1, len(scores)):
+                if combined[i][0] < combined[j][0]:
+                    if combined[i][1] > combined[j][1]:
+                        continue
+                dp[i] = max(dp[i], dp[j]+combined[i][1])
+        
+        return max(dp)
+
+        
