16cedf68be473843dbc4e74c2d91a7abf328c5c8
Yohannes Teshome <83913756+johannesteshome@users.noreply.github.com>
2023-09-29 09:52:08 +0300

    Time: 76 ms (6.21%) | Memory: 16.4 MB (33.15%) - LeetSync

---
+++ 677-map-sum-pairs/map-sum-pairs.py
@@ -0,0 +1,72 @@
+class TrieNode:
+    def __init__(self):
+        self.children = [None for _ in range(26)]
+
+        self.isEndOfWord = False
+
+        self.value = 0
+
+class MapSum:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def insert(self, key: str, val: int) -> None:
+        curr = self.root
+        for char in key:
+            pos = ord(char) - 97
+            if not curr.children[pos]:
+                curr.children[pos] = TrieNode()
+            curr = curr.children[pos]
+        curr.isEndOfWord = True
+        curr.value = val
+        
+
+    def sum(self, prefix: str) -> int:
+        print("string")
+        curr = self.root
+        queue = deque([self.root])
+        while queue:
+            for _ in range(len(queue)):
+                currs  = queue.popleft()
+                for i in range(len(currs.children)):
+                    if currs.children[i]:
+                        print(i, currs.isEndOfWord, currs.value)
+                        queue.append(currs.children[i])
+        
+        answer = 0
+        for char in prefix:
+            pos = ord(char) - 97
+            if not curr.children[pos]:
+                return answer
+            if curr.isEndOfWord:
+                answer = curr.value
+            curr = curr.children[pos]
+        if curr.isEndOfWord:
+            answer = curr.value
+        
+        def dfs(curr):
+            nonlocal answer
+            if curr.isEndOfWord:
+                answer += curr.value
+            
+            for s in curr.children:
+                if s:
+                    dfs(s)
+
+        for s in curr.children:
+            if s:
+                dfs(s)
+        return answer
+
+
+        # return 1
+
+        
+
+
+# Your MapSum object will be instantiated and called as such:
+# obj = MapSum()
+# obj.insert(key,val)
+# param_2 = obj.sum(prefix)
