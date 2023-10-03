1f15a34280c6f8bc49cd287d9736055a91ac3736
Yohannes Teshome <83913756+johannesteshome@users.noreply.github.com>
2023-10-03 16:21:01 +0300

    Time: 1476 ms (73.90%) | Memory: 26 MB (50.89%) - LeetSync

---
+++ 1906-maximize-score-after-n-operations/maximize-score-after-n-operations.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxScore(self, nums: List[int]) -> int:
+
+        @cache
+        def backtrack(index, bit):
+            ans = 0
+            for i in range(len(nums)-1):
+                if bit & (1<<i) == 0:
+                    for j in range(i+1, len(nums)):
+                        if bit & (1<<j) == 0:
+                            ans = max(ans, index*(gcd(nums[i], nums[j])) + backtrack(index+1, bit|(1<<i)|(1<<j)))
+            
+            return ans
+        
+        return backtrack(1, 0)
+                            
+        
