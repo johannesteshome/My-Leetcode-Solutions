cccd6ce668a34079b7057531dba99ab20392e6a0
Yohannes Teshome <83913756+johannesteshome@users.noreply.github.com>
2023-10-11 16:08:35 +0300

    Time: 170 ms (42.80%) | Memory: 19.2 MB (8.54%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,25 +1,32 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        # adjList = defaultdict(list)
-        # for flight in flights:
-        #     adjList[flight[0]].append((flight[1], flight[2]))
+        adjList = defaultdict(list)
+        for flight in flights:
+            adjList[flight[0]].append((flight[1], flight[2]))
         
-        dist1 = [float("inf") for _ in range(n)]
-        dist2 = [float("inf") for _ in range(n)]
-        dist1[src] = 0
-        dist2[src] = 0
+        heap = [(0, k, src)]
+        visited = set()
 
-        for _ in range(k+1):
-            # print(queue)
-            for _ in range(n - 1):
-                for u, v, w in flights:
-                    if dist1[u] != float("inf") and dist1[u] + w < dist2[v]:
-                        dist2[v] = dist1[u] + w
-            dist1 = dist2[:]
+        while heap:
+            # print(heap)
+            dist, k, node = heappop(heap) 
+
+            if (node, k) in visited:
+                continue
+            visited.add((node, k))
+            if k == -1:
+                if node == dst:
+                    return dist
+                continue
+            if node == dst:
+                return dist
+
+            for neigh in adjList[node]:
+                heappush(heap, (neigh[1] + dist, k-1, neigh[0]))
+                # if neigh[0] == dst:
+                #     return neigh[1] + dist
         
-        print(dist1)
-        if dist1[dst] == float("inf"):
-            return -1
-        return dist1[dst]
-        
-            
+        # if ans == float("inf"):
+        #     return -1
+        # return ans
+        return -1
