1bb490843dbf8bf927fd4f9bd5ffdcd0c46db2ef
Yohannes Teshome <83913756+johannesteshome@users.noreply.github.com>
2023-10-12 11:19:35 +0300

    Time: 40 ms (44.95%) | Memory: 16.3 MB (57.13%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,20 +1,19 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        res = 1
-        exp = x
-
-        binary = format(n, "b")
-
-        if binary[len(binary) - 1] == "1":
-            res = x
-        else:
-            res = 1
-        print(binary)
-        for i in range(len(binary)-2, -1, -1):
-            exp *= exp
-            if binary[i] == "1":
-                res *= exp
+        result = 1
+        c = 1
+        tn = abs(n)
+        for _ in range(32):
+            if tn & c != 0:
+                result *= x
+            x *= x
+            c <<= 1
         if n < 0:
-            return 1/res
-        return res
-        
+            return 1/result
+        return result
+        # while exponent > 0:
+        #     if exponent & 1:
+        #         result *= base
+        #     base *= base
+        #     exponent >>= 1
+        # return result
