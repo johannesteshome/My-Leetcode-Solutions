3019d5468cdebd4be750a79af4dfacefb034567b
Yohannes Teshome <83913756+johannesteshome@users.noreply.github.com>
2023-10-12 11:20:41 +0300

    Time: 45 ms (15.11%) | Memory: 16.2 MB (57.13%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,19 +1,12 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         result = 1
-        c = 1
         tn = abs(n)
-        for _ in range(32):
-            if tn & c != 0:
+        while tn > 0:
+            if tn & 1:
                 result *= x
             x *= x
-            c <<= 1
+            tn >>= 1
         if n < 0:
             return 1/result
         return result
-        # while exponent > 0:
-        #     if exponent & 1:
-        #         result *= base
-        #     base *= base
-        #     exponent >>= 1
-        # return result
