43f780eebcf9d6de9463df1fb4d0e42744b7b693
Yohannes Teshome <83913756+johannesteshome@users.noreply.github.com>
2023-12-16 07:35:12 +0300

    Time: 102 ms (15.71%) | Memory: 18.1 MB (14.50%) - LeetSync

--- 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.py
+++ 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.py
@@ -2,12 +2,8 @@ class Solution:
     def findSpecialInteger(self, arr: List[int]) -> int:
         counted = Counter(arr)
         length = len(arr)
-        percent = []
 
         for k in counted:
-            percent.append(((counted[k]/length) * 100, k))
-        
-        percent = sorted(percent, reverse=True)
-
-        return percent[0][1]
-
+            if counted[k] > length / 4:
+                return k
+    
