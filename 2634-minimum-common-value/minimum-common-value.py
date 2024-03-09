d8ff7def59e8c6bf5affb7fa2d988ed38031f459
Yohannes Teshome <83913756+johannesteshome@users.noreply.github.com>
2024-03-09 18:35:00 +0300

    Time: 348 ms (63.63%) | Memory: 35.6 MB (73.75%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.py
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -3,8 +3,10 @@ class Solution:
 
         p1 = 0
         p2 = 0
+        length1 = len(nums1)
+        length2 = len(nums2)
 
-        while p1 < len(nums1) and p2 < len(nums2):
+        while p1 < length1 and p2 < length2:
             if nums1[p1] < nums2[p2]:
                 p1 += 1
             elif nums1[p1] > nums2[p2]:
