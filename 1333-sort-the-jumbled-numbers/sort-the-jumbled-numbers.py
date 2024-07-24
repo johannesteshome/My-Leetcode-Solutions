b8bb5f75a75b5640bd810b9011386c6b1cda1fce
Yohannes Teshome <83913756+johannesteshome@users.noreply.github.com>
2024-07-24 11:04:57 +0300

    Time: 1181 ms (31.52%) | Memory: 24.8 MB (53.80%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
@@ -1,31 +1,22 @@
 class Solution:
     def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
         sortedDic = defaultdict(list)
+        numList = []
         ans = []
 
-        for num in nums:
+        for index, num in enumerate(nums):
             mapped = ""
-            tnum = num
-            while tnum != 0:
-                temp = tnum%10
-                mapped += str(mapping[temp])
-                tnum //= 10
-            if num == 0:
-                mapped += str(mapping[num])
-            
-            if mapped == "":
-                mapped = 0
-            else:
-                mapped = int(mapped[::-1])
-            sortedDic[mapped].append(num)
-        
-        print(sortedDic)
-        sortedDic = sorted(sortedDic.items())
+            tnum = str(num)
 
-        for key in sortedDic:
-            # print(key)
-            for i in range(len(key[1])):
-                # print(sortedDic[key][i])
-                ans.append(key[1][i])
+            for i in range(len(tnum)):
+                mapped += str(mapping[int(tnum[i])])
+            
+            numList.append((int(mapped), index))
+        
+        numList.sort()
+        # print(numList)
+
+        for num in numList:
+            ans.append(nums[num[1]])
 
         return ans
