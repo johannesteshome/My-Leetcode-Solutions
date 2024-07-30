c1025d0cf215f14f530492aed87545c4ef130909
Yohannes Teshome <83913756+johannesteshome@users.noreply.github.com>
2024-07-30 22:32:16 +0300

    Time: 659 ms (17.67%) | Memory: 22.1 MB (16.33%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
@@ -1,26 +1,27 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
-        deleteA = [0] * (len(s) + 1)
-        deleteB = [0] * (len(s) + 1)
+        n = len(s)
+        count_a = [0] * n
+        count_b = [0] * n
+        b_count = 0
 
-        for i in range(len(s)):
+        # First pass: compute count_b which stores the number of
+        # 'b' characters to the left of the current position.
+        for i in range(n):
+            count_b[i] = b_count
             if s[i] == "b":
-                deleteB[i+1] = deleteB[i] + 1
-            else:
-                deleteB[i+1] = deleteB[i]
-        
-        for i in range(len(s)-1,-1,-1):
+                b_count += 1
+
+        a_count = 0
+        # Second pass: compute count_a which stores the number of
+        # 'a' characters to the right of the current position
+        for i in range(n - 1, -1, -1):
+            count_a[i] = a_count
             if s[i] == "a":
-                deleteA[len(s)-1-i+1] = deleteA[len(s)-1-i] + 1
-            else:
-                deleteA[len(s)-1-i+1] = deleteA[len(s)-1-i]
-        
-        deleteA = deleteA[::-1]
+                a_count += 1
 
-        Min = float("inf")
-        for i in range(len(deleteA)):
-            Min = min(Min, deleteA[i] + deleteB[i])
-        
-        return Min
-
-        
+        min_deletions = n
+        # Third pass: iterate through the string to find the minimum deletions
+        for i in range(n):
+            min_deletions = min(min_deletions, count_a[i] + count_b[i])
+        return min_deletions
