1a0ebc1efc0a60f810431fcea43a52c55366b6e5
Yohannes Teshome <83913756+johannesteshome@users.noreply.github.com>
2024-08-27 16:45:29 +0300

    Time: 554 ms (53.48%) | Memory: 27.8 MB (99.06%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.py
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.py
@@ -1,37 +1,22 @@
 class Solution:
-    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
-        adjList = defaultdict(list)
-        visited = set()
-        probs = [float("-inf") for _ in range(n)]
-        probs[0] = -1
-
-        for i, edge in enumerate(edges):
-            adjList[edge[0]].append([edge[1], succProb[i]])
-            adjList[edge[1]].append([edge[0], succProb[i]])
-        heap = [(-1, start_node)]
-        # print(adjList)
-
-        if len(adjList[end_node]) == 0:
-            return 0
-
-        while heap:
-            # print(heap)
-            currProb, currNode = heappop(heap)
-
-            if currNode in visited:
-                continue
-            
-            visited.add(currNode)
-            # print(visited)
-            
-            
-            for neigh, prob in adjList[currNode]:
-                p = -1 * currProb * prob
-                if p > probs[neigh]:
-                    # print(neigh, p, "here")
-                    probs[neigh] = p
-                    heappush(heap, (-1 * p, neigh))
-
-        if probs[end_node] == float("-inf"):
-            return 0.0
-        return probs[end_node]
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
+        max_prob = [0] * n
+        max_prob[start] = 1
+        
+        for i in range(n - 1):
+            # If there is no larger probability found during an entire round of updates,
+            # stop the update process.
+            has_update = 0
+            for j in range(len(edges)):
+                u, v = edges[j]
+                path_prob = succProb[j]
+                if max_prob[u] * path_prob > max_prob[v]:
+                    max_prob[v] = max_prob[u] * path_prob
+                    has_update = 1 
+                if max_prob[v] * path_prob > max_prob[u]:
+                    max_prob[u] = max_prob[v] * path_prob
+                    has_update = 1
+            if not has_update:
+                break
+        
+        return max_prob[end]
