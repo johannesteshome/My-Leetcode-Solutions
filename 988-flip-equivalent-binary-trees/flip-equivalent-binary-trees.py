80b8a7d1256dde84486bfeafa3a518d9ca56e981
Yohannes Teshome <83913756+johannesteshome@users.noreply.github.com>
2024-10-24 23:13:47 +0300

    Time: 3 ms (85.18%) | Memory: 16.6 MB (58.31%) - LeetSync

--- 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.py
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.py
@@ -1,45 +1,25 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+    def flipEquiv(
+        self, root1: Optional[TreeNode], root2: Optional[TreeNode]
+    ) -> bool:
+        # Both trees are empty
         if not root1 and not root2:
             return True
-        if root1 and not root2:
+        # Just one of the trees is empty
+        if not root1 or not root2:
             return False
-        elif root2 and not root1:
+        # Corresponding values differ
+        if root1.val != root2.val:
             return False
 
-        dic1 = {}
-        dic2 = {}
-        
-        heap1 = [(root1.val, root1)]
-        heap2 = [(root2.val, root2)]
+        # Check if corresponding subtrees are flip equivalent
+        no_swap = self.flipEquiv(root1.left, root2.left) and self.flipEquiv(
+            root1.right, root2.right
+        )
 
-        while heap1 and heap2:
-            val1, node1 = heappop(heap1)
-            val2, node2 = heappop(heap2)
+        # Check if opposite subtrees are flip equivalent
+        swap = self.flipEquiv(root1.left, root2.right) and self.flipEquiv(
+            root1.right, root2.left
+        )
 
-            if val1 != val2:
-                return False
-            
-            if not node1.right and not node2.right and not node1.left and not node2.right:
-                continue
-            
-            if node1.right:
-                heappush(heap1, (node1.right.val, node1.right))
-                dic1[node1.right.val] = node1.val
-            if node1.left:
-                heappush(heap1, (node1.left.val, node1.left))
-                dic1[node1.left.val] = node1.val
-            if node2.right:
-                heappush(heap2, (node2.right.val, node2.right))
-                dic2[node2.right.val] = node2.val
-            if node2.left:
-                heappush(heap2, (node2.left.val, node2.left))
-                dic2[node2.left.val] = node2.val
-
-        return dic2 == dic1
+        return no_swap or swap
