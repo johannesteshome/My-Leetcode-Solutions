2e9c4904adea66fe28c916357811a0749d39fd7d
Yohannes Teshome <83913756+johannesteshome@users.noreply.github.com>
2025-03-05 21:48:42 +0300

    Time: 163 ms (19.55%) | Memory: 17.7 MB (75.19%) - LeetSync

---
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.py
@@ -0,0 +1,11 @@
+class Solution:
+    def coloredCells(self, n: int) -> int:
+        
+        ans = 1
+
+        for i in range(1, n+1):            
+            ans += (4 * (i - 1))
+        
+        return ans
+
+            
