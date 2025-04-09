af39be7bdb3d452ba860d9a9843c8a7cc801d571
Yohannes Teshome <83913756+johannesteshome@users.noreply.github.com>
2025-04-09 14:03:53 +0300

    Time: 0 ms (100.00%) | Memory: 18.1 MB (7.73%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
@@ -1,23 +1,27 @@
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
+        '''
+        [1,4,0,2,0,0]
+
+        '''
+
         for i in range(len(nums) - 1):
-            if nums[i] == nums[i+1] and nums[i] != 0:
+            if nums[i] == nums[i+1]:
                 nums[i] *= 2
-                nums[i+1] *= 0
+                nums[i+1] = 0
+        
+        # move the zeroes after all the operations
+        read = 0
+        write = 0
 
-        seeker = 1
-        placeholder = 0
+        while read < len(nums):
+            if nums[read] != 0:
+                temp = nums[read]
+                nums[read] = nums[write]
+                nums[write] = temp
 
-        while seeker < len(nums):
-            if nums[placeholder] == 0:
-                if nums[seeker] != 0:
-                    nums[seeker], nums[placeholder] = nums[placeholder], nums[seeker]
-                    seeker += 1
-                    placeholder += 1
-                elif nums[seeker] == 0:
-                    seeker += 1
-            elif nums[placeholder] != 0:
-                seeker += 1
-                placeholder += 1
+                write += 1
+            
+            read += 1
         
         return nums
